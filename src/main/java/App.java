cfbbce9d5c432c46ce68cfbc1aec2c9d3341ea5d
Magz8984 <collinsmuthinja@gmail.com>
2018-10-25 15:54:40 +0300

    Add Responses Test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static spark.Spark.*;
+import spark.Filter;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
@@ -16,10 +17,18 @@ public class App {
             port = 4567;
         }
         setPort(port);
+
+        after((Filter)(req,res)->{
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Methods", "GET,POST");
+          res.header("Access-Control-Allow-Credentials", "true");
+          res.status(200);
+        });
         get("/getHotelList",(req,res)->{
           return om.writeValueAsString(Hotel.getHotels());
         });
         post("/registerHotel",(req,res)->{
+          res.status(200);
           Hotel hotel=new Hotel();
           hotel.setName(req.queryParams("name"));
           hotel.setPassword(req.queryParams("password"));
@@ -29,6 +38,7 @@ public class App {
           return false;
         });
         get("/hotelOnAuth",(req,res)->{
+          res.status(200);
           Hotel hotel=new Hotel();
           if(hotel.authenticated(req.queryParams("name"),req.queryParams("password"))){
             hotel.setName(req.queryParams("name"));
@@ -38,6 +48,7 @@ public class App {
           return false;
         });
         post("/addOrder",(req,res)->{
+            res.status(200);
             Order order=new Order();
             order.setPrice(Double.parseDouble(req.queryParams("price")));
             order.setWaiter(req.queryParams("waiter"));
@@ -48,6 +59,7 @@ public class App {
             return false;
         });
         post("/registerWaiter",(req,res)->{
+           res.status(200);
            Waiter waiter=new Waiter();
            waiter.setName(req.queryParams("name"));
            waiter.setPassword(req.queryParams("password"));
@@ -57,6 +69,7 @@ public class App {
            return false;
         });
         get("/waiterOnAuth",(req,res)->{
+          res.status(200);
           Waiter waiter=new Waiter();
           if(waiter.authenticated(req.queryParams("name"),req.queryParams("password"))){
             waiter.setName(req.queryParams("name"));
@@ -68,6 +81,7 @@ public class App {
         //Hotel
         post("/addCategory",(req,res)->{
           try{
+            res.status(200);
             Category category=new Category();
             category.setName(req.queryParams("category"));
             category.setHotel(req.queryParams("hotel"));
@@ -83,6 +97,7 @@ public class App {
         });
         //Client Get Category
          get("/getCategory",(req,res)->{
+           res.status(200);
            try{
               return om.writeValueAsString(Category.getCategory(req.queryParams("hotel")));
            }catch(Exception ex){
@@ -91,11 +106,30 @@ public class App {
            return 0;
         });
         post("/addFood",(req,res)->{
+          res.status(200);
           try {
-
+            Food food=new Food();
+            food.setName(req.queryParams("name"));
+            food.setPrice(Double.parseDouble(req.queryParams("price")));
+            food.setHotel(req.queryParams("hotel"));
+            if(food.save()){
+              if(food.addToMenu(Integer.parseInt(req.queryParams("catID")))){
+                  return om.writeValueAsString(food);
+              }
+            }
           }catch (Exception e) {
             System.out.println(e.getMessage());
           }
+          return false;
+        });
+        get("/getFoods",(req,res)->{
+          res.status(200);
+          try {
+            return om.writeValueAsString(Food.getFoods(req.queryParams("hotel"),Integer.parseInt(req.queryParams("catID"))));
+          }catch(Exception ex){
+            System.out.println(ex.getMessage());
+          }
+          return 0;
         });
     }
 }
