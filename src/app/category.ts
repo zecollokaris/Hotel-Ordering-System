77120c5871e58fe2be202ac44b8e340ff3f1f723
Magz8984 <collinsmuthinja@gmail.com>
2018-10-25 20:32:58 +0300

    Add Service Data

---
+++ src/app/category.ts
@@ -0,0 +1,4 @@
+export class Category {
+  constructor(public id:number,public name:string,public hotel:string,public existing:boolean){
+  }
+}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ParamMap,ActivatedRoute} from '@angular/router'
+import {ParamMap,ActivatedRoute} from '@angular/router';
+import  {SparkService} from '../spark/spark.service';
+import {Category} from '../category';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,13 +9,22 @@ import {ParamMap,ActivatedRoute} from '@angular/router'
 })
 export class MenuComponent implements OnInit {
   hotel:string;
-
-  constructor(public route:ActivatedRoute) {
+  categorys=[];
+  category=new Category(0,"","",false);
+  constructor(public route:ActivatedRoute,public serve: SparkService) {
   }
-
   ngOnInit() {
     this.hotel=this.route.snapshot.paramMap.get("name");
-    alert(this.hotel);
+    this.categorys=this.serve.getCategorys(this.hotel);
   }
-
+  getId(name:string){
+   for(let category of this.categorys){
+     if(name==category.name){
+        return category.id;
+     }
+   }
+  }
+  getFoods(){
+    console.log(this.category.name.id);
+  }
 }
