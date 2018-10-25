ce881296865b6c5e04b7708a54cca2e86cad1c82
Wess58 <wesleymbate@gmail.com>
2018-10-25 09:33:12 +0300

    reated service to firebase

---
+++ src/app/firebase/firebase.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Http, Headers } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor(private http: HTTP) { }
+
+  getOrder(){
+    returnthis.http.get(this.???)
+  }
+
+  get
+}
